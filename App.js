91a0b47968531517979c21b7696d6fc52d5e84fd
wimpykatana <wim.katana@gmail.com>
2018-07-12 10:13:50 +0700

    basic tab

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import Chart from './screens/Chart';
+
+
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -10,6 +16,28 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default createBottomTabNavigator({
+    Home:{
+      screen: Home,
+      navigationOptions:{
+          tabBarLabel: 'Home'
+      }
+
+    },
+    Chart:{
+        screen: Chart,
+        navigationOptions:{
+            tabBarLabel: 'Chart'
+        }
+    },
+    Profile:{
+        screen: Profile,
+        navigationOptions:{
+            tabBarLabel: 'Profilegi'
+        }
+    }
+})
 
 const styles = StyleSheet.create({
   container: {
